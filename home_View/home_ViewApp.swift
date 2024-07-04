e481ae2fbef2500c3bd395541b5a9e978b73fd4e
Mohammad <mohammadshariati99990@gmail.com>
2024-07-04 19:44:06 +0300

    Initial Commit

---
+++ home_View/home_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  home_ViewApp.swift
+//  home_View
+//
+//  Created by Mohmammad Sharıatı on 4.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct home_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
